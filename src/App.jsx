44261e61f9b73a1d762000cc90d0e07e0d53f944
Facundo Guardia <contacto@facuguardia.com>
2022-11-03 00:12:55 -0300

    Ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { FaInternetExplorer } from "react-icons/fa";
 function App() {
   return (
     <>
-      <div className="container flex flex-col items-center gap-4">
+      <div className="fixed flex flex-col items-center gap-4 border-4 border-black  p-5 m-5 rounded-2xl">
         <h1 className="pt-10 pb-6 text-4xl font-bold">Mauri</h1>
         <img
           src="https://img.freepik.com/foto-gratis/lindo-perro-sonriente-gafas-sol_23-2148865714.jpg"
@@ -23,18 +23,20 @@ function App() {
             trabajo.
           </p>
         </div>
-        <button className="py-3 px-40 bg-blue-100 text-blue-500 font-bold">
-          Mi Linkdin 
-        </button>
-        <button className="w-100% py-3 px-40 bg-orange-200 text-orange-500 font-bold">
-          Trabajos experimentales
-        </button>
-        <button className="py-3 px-40 bg-orange-200 text-orange-500 font-bold">
-          Enviame un email
-        </button>
-        <button className="py-3 px-40 bg-orange-200 text-orange-500 font-bold">
-          HolaJuniors.com
-        </button>
+        <div className="grid gap-4">
+          <button className="w-50% py-3 px-[120px] bg-blue-100 text-blue-500 font-bold">
+            Mi Linkdin
+          </button>
+          <button className="py-3 bg-orange-200 text-orange-500 font-bold">
+            Trabajos experimentales
+          </button>
+          <button className="py-3 bg-orange-200 text-orange-500 font-bold">
+            Enviame un email
+          </button>
+          <button className="py-3 bg-orange-200 text-orange-500 font-bold">
+            HolaJuniors.com
+          </button>
+        </div>
 
         <div className="flex flex-row items-center gap-6 py-6 text-[30px] text-orange-700 ">
           <a href="#">
